5527c2bd5ee30a1b6011045696c4aba2672c92e6
year99 <53466034+year99@users.noreply.github.com>
2025-02-28 06:26:19 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,15 +9,16 @@ JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
 CURRENT_PID=$(pgrep -f $APP_NAME)
 
-if [ -z $CURRENT_PID ]
-then
-  echo "> 종료할것 없음."
+if [ -z "$CURRENT_PID" ]; then
+  echo "> 종료할 프로세스 없음."
 else
-  echo "> kill -9 $CURRENT_PID"
-  kill -15 $CURRENT_PID
+  echo "> 현재 실행 중인 프로세스 종료: kill -15 $CURRENT_PID"
+  kill -15 "$CURRENT_PID"
   sleep 5
 fi
 
-echo "> $JAR_PATH 배포"
-nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
+echo "> $JAR_PATH 배포 시작"
+nohup java -jar "$JAR_PATH" > /home/ubuntu/app/deploy/app.log 2>&1 &
 
+echo "> 배포 완료!"
+exit 0  # CodeDeploy에 배포 완료를 명확히 알림
